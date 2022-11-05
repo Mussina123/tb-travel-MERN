9559570ccdb83e4c382f6310e4589618e6d2f89b
Tyler Beck <softwaretpd2035@gmail.com>
2022-11-05 14:27:45 -0400

    basic nodejs server

--- server.js
+++ server.js
@@ -1,1 +1,14 @@
-console.log("hello world"); 
+const express = require('express');
+const app = express()
+// const mongoose = require('mongoose');
+// const passport = require('passport');
+// const session = require('express-session');
+// const connectDB = require('./config/database')
+const PORT = process.env.PORT || 5000
+
+
+require('dotenv').config({ path: './config/.env' });
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
+});
