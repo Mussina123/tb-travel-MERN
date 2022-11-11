8b4efa2d73acdbc0ee5870192975b20ca4542069
Tyler Beck <softwaretpd2035@gmail.com>
2022-11-10 22:11:36 -0500

    updated controller to ensure correct user had access to ONLY their posts... but error handling not perfected as it is still crashing in some exceptions

--- controller/postController.js
+++ controller/postController.js
@@ -1,9 +1,10 @@
 const Posts = require('../models/Posts');
+const User = require('../models/User');
 
 module.exports = {
     getPosts: async (req, res) => {
         try {
-            const allPosts = await Posts.find();
+            const allPosts = await Posts.find({ user: req.user.id });
             res.status(200).json(allPosts);
         } catch (err) {
             console.log(err)
@@ -17,7 +18,7 @@ module.exports = {
             }
 
             console.log(req.body);
-            const createdPost = await Posts.create({ location: req.body.location, urlOfImg: req.body.urlOfImg, cost: req.body.cost, resortName: req.body.resortName, review: req.body.review, comments: req.body.comments })
+            const createdPost = await Posts.create({ location: req.body.location, urlOfImg: req.body.urlOfImg, cost: req.body.cost, resortName: req.body.resortName, review: req.body.review, comments: req.body.comments, user: req.user.id })
             console.log('New Post has been added')
             res.status(200).json(createdPost);
         } catch (err) {
@@ -31,6 +32,19 @@ module.exports = {
             if (!post) {
                 res.status(400)
                 throw new Error('Post not found')
+            }
+
+            const currentUser = await User.findById(req.user.id)
+
+            if (!currentUser) {
+                res.status(401)
+                throw new Error('User Not Found')
+            }
+
+            // ensuring current/logged in user matches post user 
+            if (post.user.toString() !== currentUser.id) {
+                res.status(401)
+                throw new Error('User not authorized')
             }
 
             await post.remove()
@@ -49,6 +63,19 @@ module.exports = {
         if (!post) {
             res.status(400)
             throw new Error("Post not found")
+        }
+
+        const currentUser = await User.findById(req.user.id)
+
+        if (!currentUser) {
+            res.status(401)
+            throw new Error('User Not Found')
+        }
+
+        // ensuring current/logged in user matches post user 
+        if (post.user.toString() !== currentUser.id) {
+            res.status(401)
+            throw new Error('User not authorized')
         }
 
         const updatedPost = await Posts.findByIdAndUpdate(req.params.id, req.body, { new: false })
--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -1,15 +1,16 @@
 const express = require('express');
 const router = express.Router();
 // const { ensureAuth, ensureGuest } = require('../middleware/authMiddleware');
-const postController = require('../controller/post');
+const postController = require('../controller/postController');
+const { protectAuth } = require('../middleware/authMiddleware')
 
-router.get('/', /* ensureAuth, */ postController.getPosts);
+router.get('/', protectAuth, postController.getPosts);
 
-router.post('/createPost', postController.createPost);
+router.post('/createPost', protectAuth, postController.createPost);
 
-router.put('/updatePost/:id', postController.updatePost);
+router.put('/updatePost/:id', protectAuth, postController.updatePost);
 
-router.delete('/deletePost/:id', postController.deletePost);
+router.delete('/deletePost/:id', protectAuth, postController.deletePost);
 
 module.exports = router
 
