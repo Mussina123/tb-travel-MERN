6afe47dcdc22dd300947792dbc1dacf0f1784f28
Tyler Beck <softwaretpd2035@gmail.com>
2022-11-20 15:33:34 -0500

    updated login page to work with backend using global state management

--- client/src/features/auth/authService.js
+++ client/src/features/auth/authService.js
@@ -15,12 +15,25 @@ const signup = async (userData) => {
     return response.data
 }
 
+// Login User 
+const login = async (userData) => {
+    const response = await axios.post(API_URL + 'login', userData)
+
+    if (response.data) {
+        localStorage.setItem('user', JSON.stringify(response.data))
+    }
+
+    return response.data
+}
+
 const logout = async () => {
     localStorage.removeItem('user')
 }
 
 const authService = {
-    signup
+    signup,
+    logout,
+    login
 }
 
 export default authService
