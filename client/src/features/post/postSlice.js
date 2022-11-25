0246ec4b19ab33a537fe8a4259c84082b139c83d
Tyler Beck <softwaretpd2035@gmail.com>
2022-11-25 16:19:53 -0500

    created post slice and service and updated dashboard

---
+++ client/src/features/post/postSlice.js
@@ -0,0 +1,21 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+
+
+const initialState = {
+    post: [],
+    isError: false,
+    isSuccess: false,
+    isLoading: false,
+    message: ''
+}
+
+export const postSlice = createSlice({
+    name: 'post',
+    initialState,
+    reducers: {
+        reset: (state) => initialState
+    }
+})
+
+export const { reset } = postSlice.actions
+export default postSlice.reducer
