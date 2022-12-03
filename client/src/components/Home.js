3a12a387144967c4f7425c0eb64b05a2a02f6f89
Tyler Beck <softwaretpd2035@gmail.com>
2022-12-02 21:19:05 -0500

    new build and fixed mobile app styling on home page

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -8,11 +8,11 @@ const Home = () => {
     return (
         <>
 
-            <main className=' text-white'>
-                <section className='mx-8 mr-14 md:ml-8 md:mr-12 font-medium'>
+            <main className='text-white'>
+                <section className='mx-8 mr-14 md:ml-8 md:mr-12 font-medium text-center md:text-left'>
                     A community of travelers who want to make memories and explore the world TOGETHER! Join our community by signing up and posting about your travel adventures! Look at other locations others have been at for inspiration... and don't forget to GoTravel! Take a look at a few examples from our community! :D
                 </section>
-                <section className='flex justify-center'>
+                <section className='flex justify-center mt-6'>
                     <Link to='/finalhome'>
                         <button className='flex rounded-lg bg-[#E1F959] text-black px-4 hover:font-bold'>View all experiences!</button>
                     </Link>
