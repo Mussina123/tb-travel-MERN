0befc4f05da6c27466fd0f0d27aef1cd2c0d5950
Tyler Beck <softwaretpd2035@gmail.com>
2022-12-02 21:08:30 -0500

    changed style of post, added bold on left side and fixed widths in mobile and desktop:

--- client/src/components/PostItem.js
+++ client/src/components/PostItem.js
@@ -19,17 +19,35 @@ const PostItem = ({ post }) => {
                 alt='Experience visited'
                 className='urlImg rounded-lg'
                 src={post.urlOfImg} />
-            <h2>Location: {post.location}</h2>
+            <div className='flex justify-center mb-.5 mt-2'>
+                <span className='font-bold mr-2'>Location:</span><h2 className='align-end'> {post.location}</h2>
+            </div>
+            <div className='flex justify-center mb-.5'>
+                <span className='font-bold mr-2'>Resort Name: </span><h2 className='align-end'> {post.resortName}</h2>
+            </div>
+            <div className='flex justify-center mb-.5'>
+                <span className='font-bold mr-2'>Cost: </span><h2 className='align-end'> ${post.cost}</h2>
+            </div>
+            <div className='flex justify-center mb-.5'>
+                <span className='font-bold mr-2'>Review:</span><h2 className='align-end'> {post.review}</h2>
+            </div>
+            <div className='flex justify-center mb-.5'>
+                <span className='font-bold ml-4'>Comments:</span><h2 className='mx-2 mr-3 text-left max-w-xs'> {post.comments}</h2>
+            </div>
+            <div className='flex justify-center mb-.5'>
+                <span className='font-bold mr-2'>Posted At:</span><h2 className='align-end'> {new Date(post.createdAt).toLocaleString('en-US')}</h2>
+            </div>
+            {/* <h2>Location: {post.location}</h2> */}
             {/* <h2>{post.urlOfImg}</h2> */}
-            <h2>Resort Name: {post.resortName}</h2>
+            {/* <h2>Resort Name: {post.resortName}</h2>
             <h2>Cost: ${post.cost}</h2>
             <h2>Review: {post.review}</h2>
-            <h2>Comments: {post.comments}</h2>
-            <h2>Posted on: {new Date(post.createdAt).toLocaleString('en-US')}</h2>
+            <h2 className='mx-4'>Comments: {post.comments}</h2>
+            <h2>Posted on: {new Date(post.createdAt).toLocaleString('en-US')}</h2> */}
 
             {user ? (
                 <button
-                    className='trashButton'
+                    className='trashButton mt-2'
                     onClick={() => dispatch(deletePost(post._id))}
                 ><FaRegTrashAlt /></button>
             ) : <span></span>
