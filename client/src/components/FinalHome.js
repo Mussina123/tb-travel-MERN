6df7e5400d5b8009c008c4cf0fb6b1a30569de3b
Tyler Beck <softwaretpd2035@gmail.com>
2022-12-06 23:11:56 -0500

    added search functionality to finalHome and new build folder

--- client/src/components/FinalHome.js
+++ client/src/components/FinalHome.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
 // import PostForm from './PostForm'
@@ -15,8 +15,14 @@ const FinalHome = () => {
     const dispatch = useDispatch()
 
 
+    const [searchedValue, setSearchedValue] = useState('')
+
+
+
     // const { user } = useSelector((state) => state.auth)
     const { posts, isLoading, isError, message } = useSelector((state) => state.posts)
+
+    const filteredPosts = posts.filter((post) => { return post.location.toLowerCase().includes(searchedValue) })
 
     useEffect(() => {
         if (isError) {
@@ -44,14 +50,26 @@ const FinalHome = () => {
                 <h1 className='text-2xl mb-4'>
                     See Everybody's Experiences!
                 </h1>
-                {/* <p className='text-xl'>See Everybody's Experiences!</p> */}
+                <div className='mb-4'>
+                    <input
+                        className="rounded-md pl-1"
+                        type='search'
+                        name='search'
+                        id='search'
+                        value={searchedValue}
+                        placeholder="Search by location!"
+                        onChange={(event) => {
+                            const searchedInput = event.target.value.toLowerCase()
+                            setSearchedValue(searchedInput)
+                        }} />
+                </div>
             </section>
             {/* <PostForm /> */}
 
             <section >
-                {posts.length > 0 ? (
+                {filteredPosts.length > 0 ? (
                     <div className='md:grid md:grid-cols-3 md:gap-4 text-center'>
-                        {posts.map((post) => (
+                        {filteredPosts.map((post) => (
                             <PostItemFinalHome key={post._id} post={post} />
                         ))}
                     </div>
